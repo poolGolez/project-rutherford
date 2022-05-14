81d082c27d3410651a819fb2825b014edd2913c6
pool <loop.edward@gmail.com>
2022-05-14 22:03:01 +0800

    Refactor code for listing quotes

--- handlers/get_quotes.py
+++ handlers/get_quotes.py
@@ -2,14 +2,16 @@ import json
 import boto3
 
 s3 = boto3.resource('s3')
+bucket_name = 'project-rutherford-65819'
+object_key = 'quotes.json'
 
 def handler(event, context):
     print("Event", event)
-    s3_object = s3.Object('project-rutherford-65819', 'quotes.json').get()
+    s3_object = s3.Object(bucket_name, object_key).get()
     file_contents = s3_object['Body'].read().decode("utf-8")
-    print("S3 Object:", file_contents)
+    quotes_json = json.loads(file_contents)
 
     return {
         "statusCode": 200,
-        "body": json.dumps(json.loads(file_contents))
+        "body": json.dumps(quotes_json)
     }
