33b39439af0f2a160eb9e82f6dfd79c2e758409e
pool <loop.edward@gmail.com>
2022-05-14 23:42:59 +0800

    Change json format to yaml

--- cloudformation/create-s3-stack.sh
+++ cloudformation/create-s3-stack.sh
@@ -1,5 +1,5 @@
 STACK_NAME=project-rutherford-s3-stack
-TEMPLATE_LOCATION="file://${PWD}/template/project-rutherford-s3-stack.json"
+TEMPLATE_LOCATION=file://${PWD}/template/project-rutherford-s3-stack.yaml
 
 aws cloudformation validate-template --template-body $TEMPLATE_LOCATION
 aws cloudformation create-stack --stack-name $STACK_NAME --template-body $TEMPLATE_LOCATION
