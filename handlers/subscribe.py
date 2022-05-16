9e3ce7ae73948f4d3a8f6e33cd4b99a7b3899a48
pool <loop.edward@gmail.com>
2022-05-16 22:20:21 +0800

    Add logic to save subscriber email to dynamodb table

--- handlers/subscribe.py
+++ handlers/subscribe.py
@@ -1,9 +1,47 @@
 import json
+import boto3
+import os
+import uuid
+import datetime
+
+dynamodb = boto3.client("dynamodb")
+table_name = os.environ["AWS_USERS_DB_TABLE"]
+
 
 def handler(event, context):
-    print("Event", event)
+    request_body = json.loads(event['body'])
+    email = request_body['email']
+    timestamp = datetime.datetime.now()
+    item = {
+        "id": str(uuid.uuid4()),
+        "email": email,
+        "verified": False,
+        "timestamp": timestamp.isoformat()
+    }
+
+    db_response = dynamodb.put_item(
+        TableName=table_name,
+        Item=_to_db_item(item)
+    )
 
     return {
         "statusCode": 200,
-        "body": json.dumps(event)
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": json.dumps({
+            "id": str(uuid.uuid4()),
+            "email": email,
+            "timestamp":  timestamp.isoformat()
+        })
     }
+
+def _to_db_item(item):
+    db_item = dict()
+    for key, value in item.items():
+        if  isinstance(value, str):
+            db_item[key] = {"S": value}
+        elif isinstance(value, bool):
+            db_item[key] = {"BOOL": value}
+
+    return db_item
